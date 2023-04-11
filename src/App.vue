546da50e35e6fda260571ad312b5b0d8d3724c50
Ayngie <angelicareutersward@gmail.com>
2023-04-11 14:20:43 +0200

    ✨ (feat): added functionality for changing "pages", i.e. switching components in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import AddPlayers from './components/AddPlayers.vue';
 import ShowBoard from './components/ShowBoard.vue';
+
+let gameOn: boolean = false;
+
 </script>
 
 <template>
-  <AddPlayers />
-  <ShowBoard />
+  <AddPlayers v-if="gameOn === false" />
+  <ShowBoard v-else />
 </template>
 
 <style scoped></style>
