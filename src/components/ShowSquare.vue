bb250d4670afc9462b6b571206b535507998a71f
Ayngie <angelicareutersward@gmail.com>
2023-04-11 10:50:28 +0200

    ✨ (feat): added content to new component ShowSquare

---
+++ src/components/ShowSquare.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="square">
+        <h1>X/0</h1>
+    </div>
+</template>
+
+<style scoped>
+.square {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid green;
+    height: 150px;
+    width: 150px;
+}
+</style>
