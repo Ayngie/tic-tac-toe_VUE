6d30f11e750877b9bdeb6fbfedb73aac330319de
Ayngie <angelicareutersward@gmail.com>
2023-04-12 11:30:21 +0200

    ✨ (feat): begun code to reset game

---
+++ src/components/ResetGame.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+//reset game:
+function resetGame() {
+    localStorage.clear;
+}
+</script>
+
+<template>
+    <div>
+        <button type="button" @click.once="resetGame()"> Reset game
+        </button>
+
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.button {
+    margin: 2px;
+}
+
+.button:hover {
+    cursor: pointer;
+}
+
+.button:active {
+    background-color: green;
+    color: white;
+}
+</style>
